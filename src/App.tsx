b94178214277643ac662a860dd6ae1dfc98861e5
bohdanloik <bohdanloik@gmail.com>
2021-06-19 17:59:18 +0300

    - code refactoring -

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Footer } from './components/Footer/Footer';
+import { Header } from './components/Header/Header';
+import { List } from './components/List/List';
+
 
 function App() {
   return (
       <div className="App">
         <div className="todoList">
-          <div className="todoList-header">
-            <h3 className="todoList-header__title">What to Learn</h3>
-            <div className="todoList-newTaskForm">
-              <input type="text" placeholder="title"/>
-              <button>Add</button>
-            </div>
-          </div>
-          <div className="todoList-tasks">
-            <div className="todoList-task">
-              <input type="checkbox" checked={true}/>
-              <span>CSS</span>
-            </div>
-            <div className="todoList-task">
-              <input type="checkbox" checked={false}/>
-              <span>JS</span>
-            </div>
-            <div className="todoList-task">
-              <input type="checkbox" checked={false}/>
-              <span>ReactJS</span>
-            </div>
-            <div className="todoList-task">
-              <input type="checkbox" checked={true}/>
-              <span>Patterns</span>
-            </div>
-          </div>
-          <div className="todoList-footer">
-            <button>All</button>
-            <button>Completed</button>
-            <button>Active</button>
-          </div>
+          <Header />
+          <List />
+          <Footer />
         </div>
       </div>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export const Footer = () => {
+
+    return <>
+        <div className="todoList-footer">
+            <button>All</button>
+            <button>Completed</button>
+            <button>Active</button>
+          </div>
+    </>
+}
